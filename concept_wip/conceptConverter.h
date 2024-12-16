263a0a1cb76453f9ddeb7329546a5c9268f3fe3f
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-16 11:33:05 +0100

    Update conceptConverter.h

--- concept_wip/conceptConverter.h
+++ concept_wip/conceptConverter.h
@@ -3,15 +3,45 @@
 
 namespace Converters
 {
-    class ToCSV
+    class HasLogs
     {
-    public:
-        void Convert(std::string);
-    private:
-        void BracketToSpace(std::string, int);
-        void EqualiseSpaces(std::string, int);
-        void SpaceToComma(std::string, int);
-        void Polisher(std::string, int);
-        void Emitter(std::string, int);
-    };
+        class ToCSV
+        {
+        public:
+            void Convert(std::string);
+        private:
+            void BracketToSpace(std::string, int);
+            void EqualiseSpaces(std::string, int);
+            void SpaceToComma(std::string, int);
+            void Polisher(std::string, int);
+            void Emitter(std::string, int);
+        };
+
+        class ToSQL
+        {
+
+        };
+    }
+
+    class ShareLogs
+    {
+        class ToCSV
+        {
+        public:
+            void Convert(std::string);
+        private:
+            void BracketToSpace(std::string, int);
+            void EqualiseSpaces(std::string, int);
+            void SpaceToComma(std::string, int);
+            void Polisher(std::string, int);
+            void Emitter(std::string, int);
+        }
+
+
+        class ToSQL
+        {
+
+        };
+    }
+        
 };
