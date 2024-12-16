2a86d054240293b7d2826cceae0dd48378397be2
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-16 15:37:16 +0100

    Update conceptConverter.h

--- concept_wip/conceptConverter.h
+++ concept_wip/conceptConverter.h
@@ -1,21 +1,25 @@
 #pragma once
+
+#include <iostream>
 #include <string>
 
 namespace Converters
 {
     class HashLog
     {
+    public:
         class ToCSV
         {
         public:
-            void Convert(std::string);
+            std::string Convert(std::string, int);
         private:
             void BracketToSpace(std::string, int);
             void EqualiseSpaces(std::string, int);
             void SpaceToComma(std::string, int);
             void Polisher(std::string, int);
             void Emitter(std::string, int);
-
+                
+        private:
             const char space = ' ';
             const char openBracket = '[';
             const char closeBracket = ']';
@@ -26,34 +30,33 @@ namespace Converters
 
         class ToSQL
         {
-
+            
         };
-    }
+    };
 
     class ShareLog
     {
         class ToCSV
         {
         public:
-            void Convert(std::string);
+            std::string Convert(std::string);
+            std::string outputLine;
         private:
             void BracketToSpace(std::string, int);
             void EqualiseSpaces(std::string, int);
             void SpaceToComma(std::string, int);
             void Polisher(std::string, int);
-            void Emitter(std::string, int);
+            std::string Emitter(std::string, int);
 
             const char space = ' ';
             const char openBracket = '[';
             const char closeBracket = ']';
             const char comma = ',';
-        }
-
+        };
 
         class ToSQL
         {
 
         };
-    }
-        
+    };
 };
