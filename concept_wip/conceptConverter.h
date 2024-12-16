f869105d74bf7212f399e55caabf1b97f4631b5b
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-16 11:12:41 +0100

    Update conceptConverter.h

--- concept_wip/conceptConverter.h
+++ concept_wip/conceptConverter.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <string>
 
+namespace Converters
+{
+    class ToCSV
+    {
+    public:
+        void Convert(std::string);
+    private:
+        void BracketToSpace(std::string, int);
+        void EqualiseSpaces(std::string, int);
+        void SpaceToComma(std::string, int);
+        void Polisher(std::string, int);
+        void Emitter(std::string, int);
+    };
+};
