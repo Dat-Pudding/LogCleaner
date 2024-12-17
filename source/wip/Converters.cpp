29b4fd9565f99181d1627055bd234f64498ba51f
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-17 23:11:42 +0100

    Update Converters.cpp

--- source/wip/Converters.cpp
+++ source/wip/Converters.cpp
@@ -3,14 +3,14 @@
 
 // `int counter` here should start at 1
 // so the numbering works properly
-std::string Converters::HashLog::ToCSV::Convert(std::string line, int counter)
+std::string Converters::HashLog::ToCSV::Convert(std::string& line, int& counter)
 {
     BracketToSpace(line, counter);
     ++counter;
     return line;
 }
 
-void Converters::HashLog::ToCSV::BracketToSpace(std::string line, int counter)
+void Converters::HashLog::ToCSV::BracketToSpace(std::string& line, int& counter)
 {
     std::string lineCache = line;
 
@@ -26,7 +26,7 @@ void Converters::HashLog::ToCSV::BracketToSpace(std::string line, int counter)
     EqualiseSpaces(line, counter);
 }
 
-void Converters::HashLog::ToCSV::EqualiseSpaces(std::string line, int counter)
+void Converters::HashLog::ToCSV::EqualiseSpaces(std::string- line, int& counter)
 {
     int prevPos = 0;
     int currentPos = 1;
@@ -58,7 +58,7 @@ void Converters::HashLog::ToCSV::EqualiseSpaces(std::string line, int counter)
     SpaceToComma(line, counter);
 }
 
-void Converters::HashLog::ToCSV::SpaceToComma(std::string line, int counter)
+void Converters::HashLog::ToCSV::SpaceToComma(std::string& line, int& counter)
 {
     std::string lineCache = line;
 
@@ -74,7 +74,7 @@ void Converters::HashLog::ToCSV::SpaceToComma(std::string line, int counter)
     Polisher(line, counter);
 }
 
-void Converters::HashLog::ToCSV::Polisher(std::string line, int counter)
+void Converters::HashLog::ToCSV::Polisher(std::string& line, int& counter)
 {
     std::string lineCache = line;
     std::string phrase1 = ",speed,10s/60s/15m";
@@ -90,44 +90,28 @@ void Converters::HashLog::ToCSV::Polisher(std::string line, int counter)
     lineCache.erase(lineCacheStart);
     lineCache.shrink_to_fit();
 
-    for (int i = 0; i < matchStore.size(); ++i)
+    if (std::regex_search(lineCache, matchStore, pattern1))
     {
-        switch (i)
-        {
-            case 0:
-                if (std::regex_search(lineCache, matchStore, pattern1))
-                {
-                    auto p1_begin = matchStore.position(i);
-                    auto p1_end = p1_begin + phrase1.length() - 1;
-                    lineCache.erase(p1_begin, p1_end);
-                    lineCache.shrink_to_fit();
-                    break;
-                }
+        auto p1_begin = matchStore.position(i);
+        auto p1_end = p1_begin + phrase1.length() - 1;
+        lineCache.erase(p1_begin, p1_end);
+        lineCache.shrink_to_fit();
+    }
 
-            case 1:
-                if (std::regex_search(lineCache, matchStore, pattern2))
-                {
-                    auto p2_begin = matchStore.position(i);
-                    auto p2_end = p2_begin + phrase2.length() - 1;
-                    lineCache.erase(p2_begin, p2_end);
-                    lineCache.shrink_to_fit();
-                    break;
-                }
-
-            case 2:
-                if (std::regex_search(lineCache, matchStore, pattern3))
-                {
-                    auto p3_begin = matchStore.position(i);
-                    auto p3_end = p3_begin + phrase3.length();
-                    lineCache.erase(p3_begin, p3_end);
-                    lineCache.shrink_to_fit();
-                    break;
-                }
-
-            default:
-                std::cout << "Something went wrong in Polisher()" << std::endl;
-                return;
-        }
+    if (std::regex_search(lineCache, matchStore, pattern2))
+    {
+        auto p2_begin = matchStore.position(i);
+        auto p2_end = p2_begin + phrase2.length() - 1;
+        lineCache.erase(p2_begin, p2_end);
+        lineCache.shrink_to_fit();
+    }
+    
+    if (std::regex_search(lineCache, matchStore, pattern3))
+    {
+        auto p3_begin = matchStore.position(i);
+        auto p3_end = p3_begin + phrase3.length();
+        lineCache.erase(p3_begin, p3_end);
+        lineCache.shrink_to_fit();
     }
 
     line = lineCache;
@@ -135,7 +119,8 @@ void Converters::HashLog::ToCSV::Polisher(std::string line, int counter)
     Emitter(line, counter);
 }
 
-void Converters::HashLog::ToCSV::Emitter(std::string line, int counter)
+
+void Converters::HashLog::ToCSV::Emitter(std::string& line, int& counter)
 {
     std::cout << "Converted line [#" << counter << "] into CSV-compatible format" << std::endl;
     outputLine = line;
