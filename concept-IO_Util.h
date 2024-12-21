06199900a45ed71bd0912e3d5a29bc7c50bea815
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-22 00:48:57 +0100

    Update concept-IO_Util.h

--- concept-IO_Util.h
+++ concept-IO_Util.h
@@ -12,23 +12,15 @@ namespace CustomUtils
 {
     class IO
     {
-        class DefaultProcessor
+        class Processor
         {
         public:
-            
+            void DelegateLine(std::string&, std::string&, bool&); // Defaults to filterMode '-h', then follows normal procedure
             void ReadLine(std::string, std::string, bool&);
             void CheckLine(std::string, std::string, bool&);
             void WriteLine(std::string, std::string, bool&);
         private:
-            unsigned int saveCounter = 1;
-        }
-        class Processor
-        {
-        public:
-            void CheckASave(std::string&, std::string, std::string, bool&);
-            void ReadFile(std::string, std::string, std::string, bool&);
-        private:
-            unsigned int saveCounter = 1;
+            static unsigned int saveCounter = 1;
         }
     }
 }
