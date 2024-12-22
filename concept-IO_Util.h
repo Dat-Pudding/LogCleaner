7295d9402681f5cd95ac950d7b2cdad552d923c2
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-22 02:16:10 +0100

    Update concept-IO_Util.h

--- concept-IO_Util.h
+++ concept-IO_Util.h
@@ -1,12 +1,7 @@
 #pragma once
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <regex>
+
 #include <string>
-#include "AppInformation.h"
 #include "Messenger.h"
-#include "Converters.h"
 
 namespace CustomUtils
 {
@@ -16,10 +11,12 @@ namespace CustomUtils
         {
         public:
             void DelegateLine(std::string&, std::string&, bool&); // Defaults to filterMode '-h', then follows normal procedure
-            void ReadLine(std::string, std::string, bool&);
-            void CheckLine(std::string, std::string, bool&);
-            void WriteLine(std::string, std::string, bool&);
-        private:
+            void ReadLine(std::string&, std::string&, std::string&, bool&);
+            void CheckLine(std::string&, std::string&, std::string&, bool&);
+            void WriteLine(std::string&, std::string&, std::string&, bool&);
+
+        public:
+            const Messenger messenger;
             static unsigned int saveCounter = 1;
         }
     }
