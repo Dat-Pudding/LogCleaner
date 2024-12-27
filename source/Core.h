9108076d220aa1d900c0da04bfd3782ad5c15c12
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-27 12:59:23 +0100

    Update source

---
+++ source/Core.h
@@ -0,0 +1,44 @@
+namespace XMCleaner
+{
+public:
+    class Core
+    {
+    public:
+        bool Init();
+        bool Run(int, char*);
+        void Stop();
+    }
+    class IO_Utility
+    {
+    
+    }
+    class Messenger
+    {
+    public:
+        ErrorMsg_BadPath();
+        StatusMsg_SavingLine();
+    }
+    class Converter
+    {
+        class HashLog
+        {
+            class ToCSV 
+            {
+            public:
+                Convert();
+            private:
+                SymbolToSpace();
+                EqualiseSpaces();
+                SpaceToComma();
+        
+                class Polisher
+                {
+                public:
+                    Polish();
+                private:
+                    const std::string phrase1
+                };
+            }!
+        };
+    };
+};
