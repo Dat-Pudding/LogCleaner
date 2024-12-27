c639415b721d3513a031640d408405a9b86ee87b
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-27 19:46:35 +0100

    Update Core.h

--- source/Core.h
+++ source/Core.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <regex>
 #include <string>
 
 namespace XMCleaner
@@ -28,7 +32,18 @@ public:
     }
     class FileIO
     {
-    
+        class TextFileProcessor
+        {
+        public:
+            void DelegateLine(std::string&, std::string&, bool&); // Defaults to filterMode '-h', then follows normal procedure
+            void ReadLine(std::string&, std::string&, std::string&, bool&);
+            void CheckLine(std::string&, std::string&, std::string&, bool&);
+            void WriteLine(std::string&, std::string&, std::string&, bool&);
+
+        public:
+            const Messenger messenger;
+            static unsigned int saveCounter = 1;
+        }
     }
     class Messenger
     {
