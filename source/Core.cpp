066b84331568e8a582bf3c1c3e9e6d4726037c7d
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-12-27 20:55:24 +0100

    Update Core.cpp

--- source/Core.cpp
+++ source/Core.cpp
@@ -1,1 +1,16 @@
 #include "Core.h"
+
+bool XMCleaner::Core::Init()
+{
+    return true;
+}
+
+bool XMCleaner::Core::Run(int argc, char* argv[])
+{
+    return true;
+}
+
+void XMCleaner::Core::Stop()
+{
+    return;
+}
